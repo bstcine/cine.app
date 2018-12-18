f6ef965e97ff98a36cd0575fa92095598bf2c09c
itwangxiang <itwangxiang@foxmail.com>
2018-12-18 14:01:11 +0800

    webview destroy

--- app/src/main/java/com/bstcine/h5/ui/WebFragment.kt
+++ app/src/main/java/com/bstcine/h5/ui/WebFragment.kt
@@ -6,10 +6,12 @@ import android.net.http.SslError
 import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.*
+import android.widget.FrameLayout
 import com.bstcine.h5.App
 import com.bstcine.h5.Config
 import com.bstcine.h5.R
@@ -18,6 +20,19 @@ private const val ARG_HREF = "param_url"
 
 class WebFragment : Fragment() {
     private var mHref: String? = null
+
+    private var mWebContainer: FrameLayout? = null
+    private var mWebView: WebView? = null
+
+    companion object {
+        @JvmStatic
+        fun newInstance(href: String) =
+                WebFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_HREF, href)
+                    }
+                }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,13 +46,16 @@ class WebFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_web, container, false)
 
-        val web = view.findViewById<WebView>(R.id.web)
+        mWebContainer = view.findViewById(R.id.webContainer)
+        mWebView = WebView(activity!!.applicationContext)
+        mWebContainer?.addView(mWebView)
+
         // 设置允许加载混合内容
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            web.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            mWebView!!.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
         }
-        web.settings.javaScriptEnabled = true
-        web.addJavascriptInterface(object {
+        mWebView!!.settings.javaScriptEnabled = true
+        mWebView!!.addJavascriptInterface(object {
             @JavascriptInterface
             fun login() {
                 startActivity(Intent(activity, LoginActivity::class.java))
@@ -49,7 +67,7 @@ class WebFragment : Fragment() {
             }
 
         }, "Android")
-        web.webViewClient = object : WebViewClient() {
+        mWebView!!.webViewClient = object : WebViewClient() {
             /**
              * 防止加载网页时调起系统浏览器
              */
@@ -74,18 +92,29 @@ class WebFragment : Fragment() {
                 handler!!.proceed()
             }
         }
-        web.loadUrl(Config.urlBindInfo(this.mHref!!))
+        mWebView!!.loadUrl(Config.urlBindInfo(this.mHref!!))
 
         return view
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(href: String) =
-                WebFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_HREF, href)
-                    }
-                }
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.i("WebFragment", "onDestroy")
+
+        mWebContainer!!.removeAllViews()
+
+        mWebView?.clearHistory()
+
+        mWebView?.clearCache(true)
+
+        mWebView?.loadUrl("about:blank")
+
+        mWebView?.onPause()
+        mWebView?.removeAllViews()
+
+        mWebView?.destroy()
+
+        mWebView = null
     }
 }
