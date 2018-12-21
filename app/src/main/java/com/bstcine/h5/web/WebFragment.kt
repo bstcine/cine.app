f728eb80d49a79999cc1651c20079fbbc556c464
itwangxiang <itwangxiang@foxmail.com>
2018-12-21 13:52:19 +0800

    x5

--- app/src/main/java/com/bstcine/h5/web/WebFragment.kt
+++ app/src/main/java/com/bstcine/h5/web/WebFragment.kt
@@ -1,22 +1,24 @@
 package com.bstcine.h5.web
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
-import android.net.http.SslError
-import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.util.Log
+import android.support.v4.widget.SwipeRefreshLayout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.*
-import android.widget.FrameLayout
+import android.widget.Toast
 import com.bstcine.h5.CineApplication
 import com.bstcine.h5.Config
 import com.bstcine.h5.R
 import com.bstcine.h5.login.LoginActivity
+import com.bstcine.h5.widget.CWebView
+import com.tencent.smtt.sdk.WebViewClient
+import com.tencent.smtt.export.external.interfaces.SslErrorHandler
+import com.tencent.smtt.export.external.interfaces.SslError
+import com.tencent.smtt.sdk.WebView
 
 private const val ARG_HREF = "param_url"
 
@@ -24,8 +26,8 @@ class WebFragment : Fragment() {
 
     private var mHref: String? = null
 
-    private var mWebContainer: FrameLayout? = null
-    private var mWebView: WebView? = null
+    private var mRefresh: SwipeRefreshLayout? = null
+    private var mWebView: CWebView? = null
 
     companion object {
         @JvmStatic
@@ -49,36 +51,17 @@ class WebFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_web, container, false)
 
-        mWebContainer = view.findViewById(R.id.webContainer)
-        mWebView = WebView(activity!!.applicationContext)
-        mWebContainer?.addView(mWebView)
+        mRefresh = view.findViewById(R.id.refresh)
+        mWebView = CWebView(activity!!.applicationContext)
 
-        // 设置允许加载混合内容
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            mWebView!!.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
-        }
-        mWebView!!.settings.javaScriptEnabled = true
-        mWebView!!.settings.allowFileAccess = true
-        mWebView!!.settings.domStorageEnabled = true
-        mWebView!!.settings.setAppCacheEnabled(true)
-        mWebView!!.addJavascriptInterface(object {
-            @JavascriptInterface
-            fun login() {
-                startActivity(Intent(activity, LoginActivity::class.java))
-            }
+        mRefresh!!.addView(mWebView)
 
-            @JavascriptInterface
-            fun logout() {
-                CineApplication.INSTANCE.logout()
-            }
-
-        }, "Android")
+        mWebView!!.setSwipeRefreshLayout(mRefresh!!)
+        mRefresh!!.setOnRefreshListener { mWebView?.reload() }
         mWebView!!.webViewClient = object : WebViewClient() {
-            /**
-             * 防止加载网页时调起系统浏览器
-             */
-            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-                if (!url.contains("bstcine.com")) return true
+
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                if (!url!!.contains("bstcine.com")) return true
 
                 if (url.contains("bstcine.com/auth/signin")) {
                     CineApplication.INSTANCE.logout()
@@ -91,13 +74,28 @@ class WebFragment : Fragment() {
                 return true
             }
 
-            /**
-             * 允许所有SSL证书
-             */
-            override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?) {
-                handler!!.proceed()
+            override fun onReceivedSslError(p0: WebView?, p1: SslErrorHandler?, p2: SslError?) {
+                p1!!.proceed()
             }
         }
+        mWebView!!.addJavascriptInterface(object {
+            @JavascriptInterface
+            fun login() {
+                startActivity(Intent(activity, LoginActivity::class.java))
+            }
+
+            @JavascriptInterface
+            fun logout() {
+                CineApplication.INSTANCE.logout()
+            }
+
+            @JavascriptInterface
+            fun openLessonPlayWindow() {
+                Toast.makeText(context, "原生控件", Toast.LENGTH_LONG).show()
+            }
+
+        }, "Android")
+
         mWebView!!.loadUrl(Config.bindUrl(this.mHref!!))
 
         return view
@@ -106,9 +104,7 @@ class WebFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
 
-        Log.i("WebFragment", "onDestroy")
-
-        mWebContainer!!.removeAllViews()
+        mRefresh!!.removeAllViews()
 
         mWebView?.clearHistory()
 
---
+++ app/src/main/java/com/bstcine/h5/widget/CWebView.kt
@@ -0,0 +1,166 @@
+package com.bstcine.h5.widget
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Color
+import android.support.v4.content.ContextCompat
+import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.util.AttributeSet
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+
+import com.bstcine.h5.R
+import com.tencent.smtt.export.external.interfaces.IX5WebChromeClient
+import com.tencent.smtt.export.external.interfaces.JsResult
+import com.tencent.smtt.sdk.CookieSyncManager
+import com.tencent.smtt.sdk.WebChromeClient
+import com.tencent.smtt.sdk.WebSettings
+import com.tencent.smtt.sdk.WebView
+import com.tencent.smtt.sdk.WebViewClient
+
+/**
+ * Created by itwangxiang on 2018/1/10.
+ */
+
+class CWebView @JvmOverloads constructor(private val mContext: Context, attrs: AttributeSet? = null, defStyleAttr: Int = android.R.attr.webViewStyle) : WebView(mContext, attrs, defStyleAttr) {
+
+    private var mProgressBar: ProgressBar? = null
+
+    private var mSRL: SwipeRefreshLayout? = null
+
+    private val client = object : WebViewClient() {
+        /**
+         * 防止加载网页时调起系统浏览器
+         */
+        override fun shouldOverrideUrlLoading(view: WebView, url: String?): Boolean {
+            view.loadUrl(url)
+            return true
+        }
+    }
+
+    init {
+        this.webViewClient = client
+
+        initWebViewSettings()
+        this.view.isClickable = true
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initWebViewSettings() {
+        val webSetting = this.settings
+        webSetting.allowFileAccess = true
+        webSetting.layoutAlgorithm = WebSettings.LayoutAlgorithm.NARROW_COLUMNS
+        webSetting.setSupportZoom(false)
+        webSetting.builtInZoomControls = true
+        webSetting.useWideViewPort = true
+        webSetting.setSupportMultipleWindows(false)
+        webSetting.setAppCacheEnabled(true)
+        webSetting.domStorageEnabled = true
+        webSetting.javaScriptEnabled = true
+        webSetting.setGeolocationEnabled(true)
+        webSetting.setAppCacheMaxSize(java.lang.Long.MAX_VALUE)
+        webSetting.setAppCachePath(this.context.getDir("appcache", 0).path)
+        webSetting.databasePath = this.context.getDir("databases", 0).path
+        webSetting.setGeolocationDatabasePath(this.context.getDir("geolocation", 0)
+                .path)
+        webSetting.pluginState = WebSettings.PluginState.ON_DEMAND
+        CookieSyncManager.createInstance(this.context)
+        CookieSyncManager.getInstance().sync()
+
+
+        //设置进度条
+        mProgressBar = ProgressBar(mContext, null, android.R.attr.progressBarStyleHorizontal)
+        mProgressBar!!.layoutParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, 6)
+        val drawable = ContextCompat.getDrawable(mContext, R.drawable.cwebview_progress)
+        mProgressBar!!.progressDrawable = drawable
+        addView(mProgressBar)
+
+        //设置WebChromeClient
+        webChromeClient = object : WebChromeClient() {
+
+            private var mParent: ViewGroup? = null
+
+            private var mCustomView: View? = null
+
+            private var mActivity: AppCompatActivity? = null
+
+            override fun onJsAlert(webView: WebView?, s: String?, s1: String?, jsResult: JsResult?): Boolean {
+                val b = AlertDialog.Builder(mContext)
+                b.setTitle("提示")
+                b.setMessage(s1)
+                b.setPositiveButton(android.R.string.ok) { dialog, which ->
+                    jsResult!!.confirm()
+                    dialog.dismiss()
+                }
+                b.setCancelable(false)
+                b.create().show()
+                return true
+            }
+
+            override fun onJsConfirm(webView: WebView?, s: String?, s1: String?, jsResult: JsResult?): Boolean {
+                val b = AlertDialog.Builder(mContext)
+                b.setTitle("提示")
+                b.setMessage(s1)
+                b.setPositiveButton(android.R.string.ok) { dialog, which ->
+                    jsResult!!.confirm()
+                    dialog.dismiss()
+                }
+                b.setNegativeButton(android.R.string.cancel) { dialog, which ->
+                    jsResult!!.cancel()
+                    dialog.dismiss()
+                }
+                b.setCancelable(false)
+                b.create().show()
+                return true
+            }
+
+            override fun onShowCustomView(view: View?, customViewCallback: IX5WebChromeClient.CustomViewCallback?) {
+                super.onShowCustomView(view, customViewCallback)
+
+                mParent = this@CWebView.parent.parent as ViewGroup
+                mActivity = mParent?.context as AppCompatActivity
+                mActivity?.supportActionBar?.hide()
+
+                view?.setBackgroundColor(Color.parseColor("#ffffff"))
+                mParent?.addView(view)
+
+                mCustomView = view
+            }
+
+            override fun onHideCustomView() {
+                super.onHideCustomView()
+
+                mActivity?.supportActionBar?.show()
+
+                if (mCustomView != null) {
+                    mParent?.removeView(mCustomView)
+                    mCustomView = null
+                }
+            }
+
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                if (mProgressBar != null) {
+                    if (newProgress == 100) {
+                        mProgressBar!!.visibility = View.GONE
+                        if (mSRL != null) mSRL!!.isRefreshing = false
+                    } else {
+                        if (mProgressBar!!.visibility == View.GONE)
+                            mProgressBar!!.visibility = View.VISIBLE
+                        mProgressBar!!.progress = newProgress
+                    }
+                }
+
+                super.onProgressChanged(view, newProgress)
+            }
+        }
+    }
+
+    fun setSwipeRefreshLayout(srl: SwipeRefreshLayout) {
+        mSRL = srl
+    }
+}
+
